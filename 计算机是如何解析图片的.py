2e230b87e8b4c35bde9041a9e0f171d07ae4af4f
Betty <2072533959@qq.com>
2019-02-27 22:24:05 +0800

    Update 计算机是如何解析图片的.py

--- 计算机是如何解析图片的.py
+++ 计算机是如何解析图片的.py
@@ -9,3 +9,13 @@ for i in range(6):
     ax=fig.add_subplot(1,6,i+1,xticks=[],yticks=[])
     ax.imshow(X_train[i],cmap="gray")
     ax.set_title(str(y_train[i]))
+from keras.utils import np_utils
+#print first ten(integer- alued)training labels
+print("Integer- alued labels:")
+print(y——train[:10])
+#one-hot encode the labels
+y_train=np_utils.to_categorical(y_train,10)
+y_test=np_utils.to_categorical(y_test,10)
+#print first ten(one-hot) training labels
+print("One-hot labels:")
+print(y_train[:10])
